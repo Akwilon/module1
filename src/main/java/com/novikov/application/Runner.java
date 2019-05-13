80365e32edc386afba465e95214d95f357cf43cb
Daniil <d.nagib@yandex.ru>
2019-05-14 00:41:16 +0300

    updated runner

--- src/main/java/com/novikov/application/Runner.java
+++ src/main/java/com/novikov/application/Runner.java
@@ -16,33 +16,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Runner {
-    public static void main(String[] args) throws DAOException, ParserException,ServiceException {
+    public static void main(String[] args) {
         List<Toy> toyList = new ArrayList<>();
         //dao.resetDAO();
-;
-        Room r4 = RoomFactory.createRoom(25,15);
-        Toy toy = new Car(Size.LITTLE, Color.BLUE, 25);
-        Room r6 = RoomFactory.createRoom(5, 13);
+        // room with random toys within budget
+        Room r1 = RoomFactory.createRoom(25, 15);
+        //
+
+
 
         String toyCreate = "Doll{color=BLUE, size=LITTLE, value=25, ID=1510794155}";
+
+
         String toyFromFile = "src/main/resources/toy.txt";
         String par = "";
-        Service<Room> roomService = new RoomService();
-        Room r1 = RoomFactory.createRoom(15,roomService.createToyFromFile(toyFromFile));
-        //roomService.clearDAO();
-        //roomService.add(r1);
-       // roomService.add(r4);
-        //roomService.add(r6);
-        System.out.println(roomService.getAll());
-        System.out.println(r1);
-        //roomService.removeByID("59125484844461496");
-       String sort = "PRICE";
-       //roomService.sort(r5,sort);
-        roomService.sort(r1,"PRICE COLOR");
-
-        //System.out.println(roomService.query("BLUE"));
-        //System.out.println(dao);
-
+        Service<Room> roomService = null;
+        try {
+            roomService = new RoomService();
+        } catch (ServiceException e) {
+            System.out.println("Service error! Add correct input values");
+        }
+        //room with toys created from file
+        try {
+            Room r2 = RoomFactory.createRoom(15, roomService.createToyFromFile(toyFromFile));
+        } catch (ServiceException e) {
+            System.out.println("Service error! Add correct input values");
+        } catch (ParserException e) {
+            System.out.println("Wrong file");
+        }
+        //
+        //room with setted toys
+        Room r6 = RoomFactory.createRoom(5, toyList);
 
 
     }
